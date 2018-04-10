24ec547b97412f1b5147dfa31e10dd154df12c16
digantadas <diganta.das@evivehealth.com>
2018-04-10 12:51:11 +0530

    basic redux setup and demo

--- src/components/ReactTrainingHome.js
+++ src/components/ReactTrainingHome.js
@@ -5,13 +5,32 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
+import { ReactTrainingActions } from './../actions/reactTrainingActions';
+
 @connect(store => ({ reactTrainingStore: store.reactTrainingStore }))
 class ReactTrainingHome extends Component {
+
+  addButtonClick = () => {
+    const id = Math.floor(Math.random() * 1000); // NEVER USER RANDOM FOR ID. USED ONLY FOR DEMO PURPOSE
+    const newArray = [...this.props.reactTrainingStore.trainingList, {
+      id: id, // NEVER USER RANDOM FOR ID. USED ONLY FOR DEMO PURPOSE
+      value: `New Training Item ${id}`,
+    }];
+    this.props.dispatch(ReactTrainingActions.addNewTraining(newArray))
+  };
+
+  getListItem = (item) => (
+    <div key={item.id}>{item.value}</div>
+  );
+
   render() {
     return (
       <div>
         <h1>React Training</h1>
-        <h6>{this.props.reactTrainingStore.testValue}</h6>
+        <button onClick={this.addButtonClick}>Add Training</button>
+        <div>
+          {this.props.reactTrainingStore.trainingList.map(this.getListItem)}
+        </div>
       </div>
     );
   }
--- src/reducers/reactTrainingReducer.js
+++ src/reducers/reactTrainingReducer.js
@@ -2,12 +2,21 @@
  * @author evivehealth on 10/04/18.
  */
 
+import { ReactTrainingActionTypes } from './../actions/reactTrainingActions';
+
 const initialState = {
-  testValue:  'test Value from redux',
+  trainingList:  [],
 };
 
 const ReactTrainingHomeReducer = (state = initialState, action) => {
   switch(action.type) {
+
+    case ReactTrainingActionTypes.ADD_NEW_TRAINING:
+      return {
+        ...state,
+        trainingList: action.payload,
+      };
+
     default:
       return state;
   }
